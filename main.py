31c359d736feffe312499dd0852a1fae1f08dbfa
maciejborek <maciejbobrek4@gmail.com>
2023-11-06 18:01:38 +0100

    wwd

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pygame
 import sys
+import random
 
 BLACK = (0, 0, 0)
 WHITE = (255, 255, 255)
@@ -10,12 +11,32 @@ WIDTH, HEIGHT = 1350, 750
 FPS = 60
 WIN = pygame.display.set_mode((WIDTH, HEIGHT))
 
+
+
+class Actor:
+    def __init__(self, screen, strength, type):
+        self.screen=screen
+        self.strength=strength
+        self.type=type
+    def draw( self, x , y , size):
+        rect = pygame.Rect( x, y, size,size)
+        if self.type ==1:
+            color= (255,0,0)
+        else:
+            color= (0,255,0)
+        pygame.draw.rect(WIN, color, rect)
+    def find_enemy():
+        return 0
+
 def main():
     pygame.init()
     pygame.display.set_caption("Battle Simulator")
     CLOCK = pygame.time.Clock()
     WIN.fill(LIGHTGREEN)
-
+    actor1=Actor(WIN,8,1)
+    actor2=Actor(WIN,8,2)
+    actor1.draw(100,200,50)
+    actor2.draw(600,200,50)
     while True:
         drawGrid()
         CLOCK.tick(FPS)
@@ -36,3 +57,8 @@ def drawGrid():
 
 if __name__ == "__main__":
     main()
+
+
+
+
+
